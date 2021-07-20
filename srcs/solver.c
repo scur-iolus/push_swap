d2e73c8ec405ab579a0e6bdbcca5301111113780
Louis LALBA <llalba@student.42.fr>
2021-07-20 17:34:13 +0200

    20-07 01

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,33 +6,23 @@
 /*   By: llalba <llalba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 08:43:01 by llalba            #+#    #+#             */
-/*   Updated: 2021/07/19 16:04:20 by llalba           ###   ########.fr       */
+/*   Updated: 2021/07/20 17:33:38 by llalba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../libft/libft.h"
 
-static short	in_lower_half(t_list *head, int n)
+static short	in_lower_half(t_list *head, int n, int len)
 {
 	int			i;
-	static int	init_len = -1;
-	int			curr_len;
 
 	i = 0;
-	if (init_len == -1)
-		init_len = ft_lstsize(head);
-	curr_len = ft_lstsize(head);
-	if (curr_len <= init_len / 2 + init_len % 2)
-	{ // ===================================================
-		init_len = curr_len;
-		// printf("new med\n"); // ===================================================
-	} // ===================================================
 	while (head)
 	{
 		if (n < (int)head->content)
 			i++;
-		if ((10 * (long)i) >= 10 * (long)init_len / 2)
+		if ((10 * (long)i) >= 10 * (long)len / 2)
 			return (1);
 		head = head->next;
 	}
@@ -91,48 +81,59 @@ void	move_a_top(t_stacks *s)
 			execute("sa", s);
 }
 
-void	a_to_b(char c, t_stacks *s)
+void	a_to_b(t_stacks *s)
 {
-	// printf("========== Entree boucle\n"); // ====================================
-	while (!is_sorted(s) && s->stack_a->next)
+	int		init_len;
+	int		curr_len;
+
+	init_len = ft_lstsize(s->stack_a);
+	curr_len = init_len;
+	while (!is_sorted(s) && curr_len > init_len / 2 + init_len % 2)
 	{
-		if (ft_lstsize(s->stack_a) == 3)
+		if (curr_len == 3)
 			sort3_a(s);
-		if (c == 'l' && in_lower_half(s->stack_a, (int)s->stack_a->content))
+		if (ft_lstsize(s->stck_b) == 3)
+			sort3_b(s);
+		if (in_lower_half(s->stack_a, (int)s->stack_a->content))
 		{
 			execute("pb", s);
 			move_b_top(s);
 		}
-		else if (c == 'u' && in_upper_half(s->stack_a, (int)s->stack_a->content))
-			execute("pb", s);
 		else
 			execute("ra", s);
-		//print_stack(s->stack_a, "stack A"); // ====================================
-		//print_stack(s->stack_b, "stack B"); // =====================================
 	}
-	// printf("========== Fin boucle\n"); // ====================================
 }
 
 void	b_to_a(char c, t_stacks *s)
 {
-	// printf("========== Entree boucle\n"); // ====================================
-	while (!is_sorted(s) && s->stack_b->next)
+	int		init_len;
+	int		curr_len;
+
+	init_len = ft_lstsize(s->stack_b);
+	curr_len = init_len;
+	while (!is_sorted(s) && curr_len > init_len / 2 + init_len % 2)
 	{
-		if (ft_lstsize(s->stack_a) == 3)
-			sort3_a(s);
-		if (c == 'l' && in_lower_half(s->stack_b, (int)s->stack_b->content))
-			execute("pa", s);
-		else if (c == 'u' && in_upper_half(s->stack_b, (int)s->stack_b->content))
+		if (curr_len == 3)
 		{
-			execute("pa", s);
-			move_a_top(s);
+			sort3_b(s);
+			break ;
+		}
+		if (ft_lstsize(s->stck_b) == 3)
+		{
+			sort3_b(s);
+			continue ;
+		}
+		if (in_lower_half(s->stack_a, (int)s->stack_a->content))
+		{
+			execute("pb", s);
+			move_b_top(s);
 		}
 		else
-			execute("rb", s);
-		//print_stack(s->stack_a, "stack A"); // ====================================
-		//print_stack(s->stack_b, "stack B"); // =====================================
+			execute("ra", s);
 	}
-	// printf("========== Fin boucle\n"); // ====================================
+
+https://www.youtube.com/watch?v=egU3YD8OKbw
+
 }
 
 
@@ -158,3 +159,4 @@ void	solve(t_stacks *s)
 
 }
 */
+
